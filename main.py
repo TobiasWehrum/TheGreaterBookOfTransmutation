af0dba57b48f249af150a70eeb7986341ef72eb9
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2015-11-22 21:30:12 +0100

    Different quantity types for different word types

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from recipe import ActionSimple
 from recipe import ActionConsuming
 from recipe import ActionGenerating
 from recipe import ActionTransforming
+import nltk
+from nltk.corpus import brown
 
 
 def main():
@@ -20,14 +22,23 @@ def main():
     # material_names = ["elephant", "tomato", "window", "poison", "water"]
 
     quantity_type_countable = QuantityType("{amount} {material}", "{amount} {material_plural}", True)
-    quantity_type_gram = QuantityType("{amount} gram of {material}", "{amount} grams of {material}", False)
-    quantity_types = [quantity_type_countable, quantity_type_gram]
+    quantity_type_ounces = QuantityType("{amount} ounce of {material}", "{amount} ounces of {material}", False)
+    quantity_type_spoonful = QuantityType("{amount} spoonful of {material}", "{amount} spoonful of {material}", False)
+    quantity_type_idea = QuantityType("{amount} idea of {material}", "{amount} ideas of {material}", True)
+    quantity_type_concept = QuantityType("{amount} clear concept of {material}", "{amount} clear concepts of {material}", True)
+    quantity_type_notion = QuantityType("{amount} vague notion of {material}", "{amount} vague notions of {material}", True)
+
+    quantity_type_noun = [quantity_type_countable, quantity_type_ounces, quantity_type_spoonful]
+    quantity_type_adjective_and_verb = [quantity_type_idea, quantity_type_concept, quantity_type_notion]
+    quantity_types = {WORD_TYPE_NOUN: quantity_type_noun,
+                      WORD_TYPE_ADJECTIVE: quantity_type_adjective_and_verb,
+                      WORD_TYPE_VERB_PRESENT: quantity_type_adjective_and_verb}
 
     tool_type_vessel = ToolType(["cauldron"]) #ToolType(["cauldron", "container", "vessel"])
     tool_type_vessel.add(ActionConsuming("Put {material} into {tool}"))
     tool_type_vessel.add(ActionSimple("[Stir|Heat] {tool}").cooldown(2))
     tool_type_vessel.add(ActionSimple("Let {tool} cool down").cooldown(2))
-    tool_type_vessel.add(ActionGenerating("Pour out the mixture from {tool} to get the {result}", "{tool} mixture \"{contents}\"", [quantity_type_gram], True).cooldown(2))
+    tool_type_vessel.add(ActionGenerating("Pour out the mixture from {tool} to get the {result}", "{tool} mixture \"{contents}\"", [quantity_type_ounces, quantity_type_spoonful], True).cooldown(2))
 
     tool_type_smashing = ToolType(["stone"]) #stone
     tool_type_smashing.add(ActionTransforming("Smash {material} with the {tool}", "smashed {material}").cooldown(2))
@@ -50,10 +61,12 @@ def create_recipe(end_product, material_names, quantity_types, tool_types, tool_
     recipe = Recipe(end_product)
 
     for material_name in random.sample(material_names, min(4, len(material_names))):
-        quantity_type = random.choice(quantity_types)
-        amount = quantity_type.random_amount()
+        material_word_type = find_most_common_word_type(material_name)
+        if material_word_type in quantity_types:
+            quantity_type = random.choice(quantity_types[material_word_type])
+            amount = quantity_type.random_amount()
 
-        recipe.add_material(Material(material_name, amount, quantity_type))
+            recipe.add_material(Material(material_name, amount, quantity_type))
 
     for tool_type in random.sample(tool_types, min(2, len(tool_types))):
         tool = Tool(tool_type)
@@ -68,6 +81,32 @@ def create_recipe(end_product, material_names, quantity_types, tool_types, tool_
     print()
     recipe.print()
 
+
+DEBUG = True
+
+WORD_TYPE_UNKNOWN = ""
+WORD_TYPE_NOUN = "NN"
+WORD_TYPE_ADJECTIVE = "JJ"
+# WORD_TYPE_VERB = "VB"
+WORD_TYPE_VERB_PRESENT = "VBG"
+WORD_TYPES = [WORD_TYPE_NOUN, WORD_TYPE_ADJECTIVE, WORD_TYPE_VERB_PRESENT]
+
+
+def find_most_common_word_type(word):
+    result = nltk.FreqDist(t for w, t in brown.tagged_words() if w.lower() == word).most_common()
+    if len(result) > 0:
+        result_type = result[0][0]
+        for word_type in WORD_TYPES:
+            if result_type.startswith(word_type):
+                return word_type
+
+        if DEBUG:
+            print("[find_most_common_word_type] Unknown word type: " + result_type + " (for " + word + ")")
+    else:
+        if DEBUG:
+            print("[find_most_common_word_type] Unknown word: " + word)
+
+    return WORD_TYPE_UNKNOWN
 
 # Only run if we are the main program, not an import.
 if __name__ == "__main__": main()
